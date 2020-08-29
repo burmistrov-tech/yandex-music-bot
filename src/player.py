325c5464d09b67e264cec91f5adf01a16b5d805b
nburmistrov <nick.2001.60@gmail.com>
2020-08-29 19:41:02 +0600

    bugfix; removed extra characters; add whitespaces

--- src/player.py
+++ src/player.py
@@ -7,14 +7,16 @@ from discord.voice_client import VoiceClient
 from discord.ext.commands import Bot
 
 from .audio import Audio
-from .extended.errors import PlayerInvalidState, PlayerInvalidVolume, PlayerQueueEmpty, MissingInChannel
+from .extended.errors import PlayerInvalidState, PlayerInvalidVolume, \
+    PlayerQueueEmpty, MissingInChannel
 
-class Player():        
+
+class Player():
     def __init__(self, voice_client: VoiceClient):
         self.voice_client = voice_client
         self.audio_list = list()
         self._state = asyncio.Event()
-        self._volume = 0.5 
+        self._volume = 0.5
 
     def toogle_next(self, error: Exception = None, *args):
         self._state.set()
@@ -23,37 +25,39 @@ class Player():
 
     async def _run(self):
         while self.audio_list:
-            self._state.clear()   
+            self._state.clear()
             audio = self.audio_list.pop(0)
             source = audio.get()
-            source.volume = self.volume         
-            self.voice_client.play(source, after = self.toogle_next)
+            source.volume = self.volume
+            self.voice_client.play(source, after=self.toogle_next)
             print(f'voice client начал проигрывание {audio.track.title}')
-            await self._state.wait() 
+            await self._state.wait()
 
     @property
     def volume(self):
         return self._volume
-    
+
     @volume.setter
     def volume(self, value: float):
-        if not 0 <= value <= 100:                    
-            raise PlayerInvalidVolume('The value have to be between 0 and 100')        
-        
+        if not 0 <= value <= 100:
+            raise PlayerInvalidVolume('The value have to be between 0 and 100')
+
         self._volume = value / 100
-        if self.is_playing():
+        if self.is_playing:
             self.voice_client.source.volume = self.volume
-           
+
     @property
     def is_playing(self) -> bool:
         return self.voice_client.is_playing()
+
     @property
-    def is_paused(self) -> bool:                    
+    def is_paused(self) -> bool:
         return self.voice_client.is_paused()
+
     @property
-    def is_empty(self) -> bool:        
+    def is_empty(self) -> bool:
         return len(self.audio_list) > 0
-    
+
     async def play(self, audio: Audio):
         self.audio_list.append(audio)
 
@@ -62,16 +66,16 @@ class Player():
 
     async def playlist(self, audio: List[Audio]):
         self.audio_list.extend(audio)
-        
+
         if not self.is_playing:
-            await self._run()   
+            await self._run()
 
     async def pause(self):
         if self.is_playing:
             raise PlayerInvalidState('Bot is not playing')
 
         self.voice_client.pause()
-    
+
     async def resume(self):
         if self.is_paused:
             raise PlayerInvalidState('Bot has not paused')
@@ -85,8 +89,8 @@ class Player():
             if self.is_playing or self.is_paused:
                 self.voice_client.stop()
             else:
-                raise PlayerInvalidState('Bot is not playing or paused')        
-            
+                raise PlayerInvalidState('Bot is not playing or paused')
+
     async def skip(self):
         if self.is_playing or self.is_paused:
             self.voice_client.stop()
@@ -101,17 +105,18 @@ class Player():
 
     async def queue(self, amount: int = 10) -> List[Audio]:
         return self.audio_list[:amount]
-        
+
     async def clear(self):
         if self.is_empty:
             raise PlayerQueueEmpty()
 
         self.audio_list.clear()
 
+
 class PlayerPool():
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.players = dict()        
+        self.players = dict()
 
     def get(self, guild, *args) -> Player:
         from discord.utils import get
@@ -120,7 +125,7 @@ class PlayerPool():
         if voice_client is None:
             raise MissingInChannel('Bot not in the channel')
 
-        player = self.players.get(guild)        
+        player = self.players.get(guild)
         if not player or player.voice_client != voice_client:
             player = Player(voice_client)
             self.players[guild] = player
